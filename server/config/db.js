178c58aa727b9b6ad8e410545d298a381059b89b
Shakiba448 <shakiba448@gmail.com>
2021-03-31 06:35:45 +0600

    WIP on MYSQL

--- server/config/db.js
+++ server/config/db.js
@@ -2,19 +2,25 @@ import mysql from "mysql";
 import dotenv from "dotenv";
 dotenv.config();
 
-const connectDB = () => {
-  const conn = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    multipleStatements: true,
-  });
+const connectDB = async () => {
+  try {
+    const conn = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      multipleStatements: true,
+    });
 
-  conn.connect((err) => {
-    if (err) throw err;
-    console.log("MYSQL connected".cyan.underline);
-  });
+    conn.connect((err) => {
+      if (err) throw err;
+      console.log("MYSQL connected".cyan.underline);
+    });
+  } catch (error) {
+    console.error(`Error : ${error.message}`.red.underline.bold);
+    console.log(error);
+    connection.end();
+  }
 };
 
 export default connectDB;
